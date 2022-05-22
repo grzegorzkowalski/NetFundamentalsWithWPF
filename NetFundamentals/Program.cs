073f28d2d4156af16b032547ae746ed696c6ade4
grzegorzkowalski <grzegorzkowalski@yahoo.com>
2022-05-22 23:34:10 +0200

    Task1

--- NetFundamentals/Program.cs
+++ NetFundamentals/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var tasks = new Tasks();
+            tasks.Task1();
         }
     }
 }
---
+++ NetFundamentals/Tasks.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFundamentals
+{
+    internal class Tasks
+    {
+        internal void Task1()
+        {
+            Console.WriteLine("Jak masz na imię?");
+            var name = Console.ReadLine();
+            Console.WriteLine(name);
+        }
+    }
+}
